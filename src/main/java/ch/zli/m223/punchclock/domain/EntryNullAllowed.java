e74d5eb7ed99b6ce06090f1a52d87c0d480eb028
tihuep <timi.hueppi@gmx.ch>
2020-09-19 16:47:36 +0200

    minor change

---
+++ src/main/java/ch/zli/m223/punchclock/domain/EntryNullAllowed.java
@@ -0,0 +1,23 @@
+package ch.zli.m223.punchclock.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class EntryNullAllowed extends Entry {
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(nullable = true)
+    private LocalDateTime checkIn;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(nullable = true)
+    private LocalDateTime checkOut;
+}
